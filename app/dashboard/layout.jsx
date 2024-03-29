edc18dfff6fae0b21a8f9d7bce5f7b219cf72304
patrWan <patricio.gonzalez.camilo@gmail.com>
2024-03-29 16:50:38 -0300

    fix: background pattern cover all screen

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -33,7 +33,7 @@ export default function ({ children }) {
             </div>
 
 
-            <div className=" w-full h-full bg-teal-600"   >
+            <div className="w-full min-h-max bg-teal-600"   >
                 <div className="bg-repeat w-full h-full text-primary-100 heropattern-overlappinghexagons-teal-900" >
                     <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
                         {children}
